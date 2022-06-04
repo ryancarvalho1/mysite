ed8cf30b188ccdb849cef1ed89f2b64f3c62bd3d
ryancarvalho1 <105951173+ryancarvalho1@users.noreply.github.com>
2022-06-03 22:04:38 -0300

    Add files via upload

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,8 +1,16 @@
 import imp
+from unittest import result
 from django.urls import URLPattern, path
 
 from . import views
 
 urlpatterns = [
     path ('', views.index, name='index'),
+    # /polls/1/
+    path('<int:question_id>/', views.detail, name='detail'),
+    #/polls/1/results/
+    path('<int:question_id>/results', views.results, name='results'),
+    #/polls/1/vot/
+    path('<int:question_id>/vote', views.vote, name='vote')
+
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,16 @@
+from multiprocessing import context
+from re import template
+from urllib import response
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
-# Create your views here.
+    # Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pud_date')[:1]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
